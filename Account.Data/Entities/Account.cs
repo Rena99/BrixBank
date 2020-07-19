3a0d142a3722a6c9843dec1c9bcd29799343af10
Rena <renamarkiewitz@gmail.com>
2020-07-19 11:14:37 +0300

    added entities

---
+++ Account.Data/Entities/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Account.Data.Entities
+{
+    public class Account
+    {
+        public Guid AccountId { get; set; }
+        public DateTime OpenDate { get; set; }
+        public double Balance { get; set; }
+        public Guid CustomerId { get; set; }
+        public virtual Customer Customer { get; set; }
+    }
+}
---
+++ Account.Data/Entities/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Account.Data.Entities
+{
+    public class Customer
+    {
+        public Guid CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
